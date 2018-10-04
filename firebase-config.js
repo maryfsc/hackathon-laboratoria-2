aeae2610c354cca7689783673d36e6b467e75ee2
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-10-04 13:53:34 -0300

    firebase funcionando

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC6qD3VP_hcMOvwf3ZZQl_rHwh4bKVTokM",
-  authDomain: "rotaseclima.firebaseapp.com",
-  databaseURL: "https://rotaseclima.firebaseio.com",
-  projectId: "rotaseclima",
-  storageBucket: "rotaseclima.appspot.com",
-  messagingSenderId: "479177718069"
-};
-firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC6qD3VP_hcMOvwf3ZZQl_rHwh4bKVTokM",
+    authDomain: "rotaseclima.firebaseapp.com",
+    databaseURL: "https://rotaseclima.firebaseio.com",
+    projectId: "rotaseclima",
+    storageBucket: "rotaseclima.appspot.com",
+    messagingSenderId: "479177718069"
+  };
+  firebase.initializeApp(config);
